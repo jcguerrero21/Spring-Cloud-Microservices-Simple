4677e722d524abe3a13dcbcc28e8a87b3edcc3f3
juan carlos <jotaceguerrerom21@gmail.com>
2019-09-22 23:15:07 +0200

    ADD EUREKA SERVER

--- MicroService-Productos/src/main/java/jotace/example/cloud/CloudProjectApplication.java
+++ MicroService-Productos/src/main/java/jotace/example/cloud/CloudProjectApplication.java
@@ -2,7 +2,9 @@ package jotace.example.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CloudProjectApplication {
 
--- MicroServiceItems/src/main/java/jotace/spring/cloud/items/demo/ServicioItemsApplication.java
+++ MicroServiceItems/src/main/java/jotace/spring/cloud/items/demo/ServicioItemsApplication.java
@@ -2,9 +2,11 @@ package jotace.spring.cloud.items.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @RibbonClient(name="servicio-productos")
 @EnableFeignClients
 @SpringBootApplication
